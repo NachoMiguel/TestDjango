7312651f838c55b63df93ea02fc3d6cf4d105a44
Ignacio Miguel <Ignacio.X.Miguel.-ND@disney.com>
2015-08-22 01:06:08 -0300

    Trabajo terminado desde casa, terminado el tutorial

--- wiki/views.py
+++ wiki/views.py
@@ -18,6 +18,7 @@ class IndexView(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     #__lte_ less then or equal, en este case : menor o igual al dia de hoy
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'wiki/detail.html'
